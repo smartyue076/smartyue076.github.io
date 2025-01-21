fc7738652526e2454a4688a61c1d8b5598d65a69
SmartYue076 <martinxu076@163.com>
2025-01-21 19:10:33 +0800

    Site updated: 2025-01-21 19:10:32

--- js/main.js
+++ js/main.js
@@ -56,9 +56,9 @@ $(document).ready(function() {
      */
     if (menu.length) {
       $(window).on("scroll", function() {
-        var topDistance = menu.offset().top;
-
-        // hide only the navigation links on desktop
+	var topDistance = document.documentElement.scrollTop;// menu.offset().top;
+        
+	// hide only the navigation links on desktop
         if (!nav.is(":visible") && topDistance < 50) {
           nav.show();
         } else if (nav.is(":visible") && topDistance > 100) {
